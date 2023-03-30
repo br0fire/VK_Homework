e3248a8ce8952e8897b9cf1ec055b175112db939
br0fire <tgrozny@yandex.ru>
2023-03-30 20:38:22 +0300

    added

---
+++ Task3/solve.py
@@ -0,0 +1,43 @@
+from itertools import zip_longest
+
+
+class CustomList(list):
+    def __init__(self, lst):
+        self.sum = sum(lst)
+        super().__init__(lst)
+
+    def __add__(self, other):
+        res = zip_longest(self, other, fillvalue=0)
+        return CustomList(map(lambda x: x[0] + x[1], res))
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        res = zip_longest(self, other, fillvalue=0)
+        return CustomList(map(lambda x: x[0] - x[1], res))
+
+    def __rsub__(self, other):
+        res = zip_longest(self, other, fillvalue=0)
+        return CustomList(map(lambda x: x[1] - x[0], res))
+
+    def __lt__(self, other):
+        return self.sum < other.sum
+
+    def __le__(self, other):
+        return self.sum <= other.sum
+
+    def __gt__(self, other):
+        return self.sum > other.sum
+
+    def __ge__(self, other):
+        return self.sum >= other.sum
+
+    def __eq__(self, other):
+        return self.sum == other.sum
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return super().__str__() + ", sum=" + str(self.sum)
