3d4888d37713df0963682a4b3a3b0dd83093cd7d
br0fire <tgrozny@yandex.ru>
2023-04-14 23:54:02 +0300

    finished

--- 04/metaclass.py
+++ 04/metaclass.py
@@ -1,9 +1,43 @@
+from functools import wraps
+
+
+def is_magic(name: str) -> bool:
+    return name.startswith("__") and name.endswith("__")
+
+
+def custom_dct(attrs):
+    custom_attrs = {}
+    for attr_name, attr_value in attrs.items():
+        if is_magic(attr_name):
+            custom_attrs[attr_name] = attr_value
+        else:
+            custom_attrs["custom_" + attr_name] = attr_value
+    return custom_attrs
+
+
 class CustomMeta(type):
-    def __new__(mcs, name, bases, attr_dict):
-        new_attr_dict = {}
-        for nme, val in attr_dict.items():
-            new_attr_dict[f'custom_{nme}'] = val
-        return super().__new__(mcs, name, bases, new_attr_dict)
+    def __new__(mcs, name, bases, attrs):
+
+        def _setattr(self, name, value):
+            if not is_magic(name):
+                name = "custom_"+name
+            object.__setattr__(self, name, value)
+
+        def override_init(function):
+            @wraps(function)
+            def _wrapper(*args, **kwargs):
+                cls.__setattr__ = object.__setattr__
+                function(*args, **kwargs)
+                cls.__setattr__ = _setattr
+            return _wrapper
+        cls = super().__new__(mcs, name, bases, custom_dct(attrs))
+        cls.__init__ = override_init(cls.__init__)
+        return cls
+
+    def __call__(cls, *args, **kwargs):
+        ret = super().__call__(*args, **kwargs)
+        ret.__dict__ = custom_dct(ret.__dict__)
+        return ret
 
 
 class CustomClass(metaclass=CustomMeta):
--- 04/test.py
+++ 04/test.py
@@ -1,5 +1,6 @@
 import unittest
 from descriptors import ChipTantrix
+from metaclass import CustomClass
 
 
 class TestChipTantrix(unittest.TestCase):
@@ -42,6 +43,50 @@ class TestChipTantrix(unittest.TestCase):
     def test_solid_lines_invalid_boolean(self):
         with self.assertRaises(ValueError):
             _ = ChipTantrix(5, 1, (True, "False", True))
+
+
+class TestCustomClass(unittest.TestCase):
+
+    def setUp(self):
+        self.inst = CustomClass()
+
+    def test_custom_x(self):
+        self.assertEqual(CustomClass.custom_x, 50)
+
+    def test_custom_class_x(self):
+        with self.assertRaises(AttributeError):
+            self.inst.x
+
+    def test_custom_val(self):
+        self.assertEqual(self.inst.custom_val, 99)
+
+    def test_custom_line(self):
+        self.assertEqual(self.inst.custom_line(), 100)
+
+    def test_str(self):
+        self.assertEqual(str(self.inst), "Custom_by_metaclass")
+
+    def test_inst_x(self):
+        with self.assertRaises(AttributeError):
+            self.inst.x
+
+    def test_inst_val(self):
+        with self.assertRaises(AttributeError):
+            self.inst.val
+
+    def test_inst_line(self):
+        with self.assertRaises(AttributeError):
+            self.inst.line()
+
+    def test_inst_yyy(self):
+        with self.assertRaises(AttributeError):
+            self.inst.yyy
+
+    def test_dynamic(self):
+        self.inst.dynamic = "added later"
+        self.assertEqual(self.inst.custom_dynamic, "added later")
+        with self.assertRaises(AttributeError):
+            self.inst.dynamic
 
 
 if __name__ == '__main__':
