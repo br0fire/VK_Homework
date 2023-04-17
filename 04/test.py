c46ee882d0bfe5b935c837cafa893e5c5901f1ec
br0fire <tgrozny@yandex.ru>
2023-04-17 17:28:06 +0300

    second try

--- 04/test.py
+++ 04/test.py
@@ -5,14 +5,23 @@ from metaclass import CustomClass
 
 class TestChipTantrix(unittest.TestCase):
 
-    def test_area_positive(self):
+    def test_area_positive_int(self):
         chip = ChipTantrix(5, 1, (True, False, True))
         self.assertEqual(chip.area, 5)
-        chip.area = 5.5
-        try:
-            chip.area = 1.1
-        except ValueError:
-            self.fail('Float error')
+        chip.area = 3
+        self.assertEqual(chip.area, 3)
+        with self.assertRaises(ValueError):
+            chip.area = []
+        self.assertEqual(chip.area, 3)
+
+    def test_area_positive_float(self):
+        chip = ChipTantrix(5.5, 1, (True, False, True))
+        self.assertEqual(chip.area, 5.5)
+        chip.area = 2.2
+        self.assertEqual(chip.area, 2.2)
+        with self.assertRaises(ValueError):
+            chip.area = ()
+        self.assertEqual(chip.area, 2.2)
 
     def test_area_negative(self):
         with self.assertRaises(ValueError):
@@ -25,20 +34,33 @@ class TestChipTantrix(unittest.TestCase):
     def test_label_valid(self):
         chip = ChipTantrix(5, 1, (True, False, True))
         self.assertEqual(chip.label, 1)
+        chip.label = 3
+        self.assertEqual(chip.label, 3)
 
     def test_label_invalid(self):
+
         with self.assertRaises(ValueError):
             _ = ChipTantrix(5, 11, (True, False, True))
+        chip = ChipTantrix(5, 10, (True, False, True))
+        with self.assertRaises(ValueError):
+            chip.label = 0
+        self.assertEqual(chip.label, 10)
 
     def test_solid_lines_valid(self):
         chip = ChipTantrix(5, 1, (True, False, True))
         self.assertEqual(chip.solid_lines, (True, False, True))
+        chip.solid_lines = (False, False, False)
+        self.assertEqual(chip.solid_lines, (False, False, False))
 
     def test_solid_lines_invalid_tuple(self):
         with self.assertRaises(ValueError):
             _ = ChipTantrix(5, 1, (True, False))
         with self.assertRaises(ValueError):
             _ = ChipTantrix(5, 1, [True, False, True])
+        chip = ChipTantrix(5, 1, (True, False, True))
+        with self.assertRaises(ValueError):
+            chip.solid_lines = 5
+        self.assertEqual(chip.solid_lines, (True, False, True))
 
     def test_solid_lines_invalid_boolean(self):
         with self.assertRaises(ValueError):
