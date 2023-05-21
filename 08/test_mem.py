8ff00e22df21e0a068b17b3a8d273f38b694176c
br0fire <tgrozny@yandex.ru>
2023-05-22 00:36:41 +0300

    second commit

--- 08/test_mem.py
+++ 08/test_mem.py
@@ -3,18 +3,17 @@ from classes import A_init, B_init, C_init
 from classes import A_change, B_change, C_change
 
 
-
 if __name__ == '__main__':
 
-    n = 100_000
-    profile(A_init)(n)
+    n = 50_000
+    A_arr = profile(A_init)(n)
 
-    profile(B_init)(n)
+    B_arr = profile(B_init)(n)
 
-    profile(C_init)(n)
+    C_arr = profile(C_init)(n)
 
-    profile(A_change)(n)
+    profile(A_change)(A_arr)
 
-    profile(B_change)(n)
+    profile(B_change)(B_arr)
 
-    profile(C_change)(n)
+    profile(C_change)(C_arr)
--- 08/test_time.py
+++ 08/test_time.py
@@ -5,29 +5,29 @@ from classes import A_change, B_change, C_change
 
 if __name__ == '__main__':
 
-    n = 100_000
+    n = 5_000_000
     start_time = time.time()
     print('Class init:')
-    A_init(n)
+    A_arr = A_init(n)
     print("Time taken by A: ", time.time() - start_time)
 
     start_time = time.time()
-    B_init(n)
+    B_arr = B_init(n)
     print("Time taken by B: ", time.time() - start_time)
 
     start_time = time.time()
-    C_init(n)
+    C_arr = C_init(n)
     print("Time taken by C: ", time.time() - start_time)
     print('Read and write attr:')
     # Измеряем время чтения/изменения атрибутов
     start_time = time.time()
-    A_change(n)
+    A_change(A_arr)
     print("Time taken by A: ", time.time() - start_time)
 
     start_time = time.time()
-    B_change(n)
+    B_change(B_arr)
     print("Time taken by B: ", time.time() - start_time)
 
     start_time = time.time()
-    C_change(n)
+    C_change(C_arr)
     print("Time taken by C: ", time.time() - start_time)
