020692b5a2e7f32691e3742639040d1625dde51e
br0fire <tgrozny@yandex.ru>
2023-05-27 13:59:57 +0300

    second commit

--- 06/client.py
+++ 06/client.py
@@ -1,7 +1,49 @@
 import argparse
+import socket
+import threading
 
-if __name__ == '__main__':
+
+def send_requests(urls_file, num_threads, host, port):
+    """
+    Отправляет запросы на сервер и обрабатывает ответы в несколько потоков.
+    """
+    urls = urls_file.readlines()
+    urls_per_thread = len(urls) // num_threads
+    threads = []
+
+    lock = threading.Lock()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        for i in range(num_threads):
+            start_index = i * urls_per_thread
+            end_index = start_index + urls_per_thread if i != num_threads - 1 else None
+            urls_chunk = urls[start_index:end_index]
+
+            thread = threading.Thread(target=send_requests_in_chunk, args=(s, urls_chunk, lock))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+
+def send_requests_in_chunk(s, urls, lock):
+    """
+    Отправляет запросы на сервер и обрабатывает ответы для конкретного чанка URL'ов.
+    """
+    for url in urls:
+        #lock.acquire()
+        url = url.strip()
+        s.sendall(url.encode())
+        response = s.recv(1024).decode()
+        #lock.release()
+        print(response)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('file')
-    parser.add_argument('M')
-    print(parser.parse_args())
+    parser.add_argument("-num_threads", type=int, default=5)
+    parser.add_argument("-urls_file", type=argparse.FileType("r"), default='links.txt')
+    args = parser.parse_args()
+    send_requests(args.urls_file, args.num_threads, 'localhost', 8000)
--- 06/server.py
+++ 06/server.py
@@ -4,35 +4,108 @@ import json
 import re
 from collections import Counter
 import argparse
+import socket
+import threading
+from queue import Queue
 
 
-def get_words(url, k):
-    # Отправляем GET-запрос по указанному URL
-    response = requests.get(url)
+class Worker(threading.Thread):
+    def __init__(self, id, task_queue, stats, stats_lock, k):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.task_queue = task_queue
+        self.stats = stats
+        self.stats_lock = stats_lock
+        self.k = k
 
-    # Парсим HTML-код и получаем текст
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = soup.get_text()
+    def run(self):
+        while True:
+            # получаем задачу из очереди
+            conn, url = self.task_queue.get()
+            try:
+                # обрабатываем url и отправляем результат клиенту
+                result = self.get_words(url)
+                response = f"{url}: {result}".encode("utf-8")
+                conn.sendall(response)
+            except Exception as e:
+                print(f"Error processing URL {url}: {e}")
+                conn.sendall(f"{url}: error".encode("utf-8"))
+            finally:
+                # сообщаем, что задача выполнена
+                self.task_queue.task_done()
 
-    # Удаляем спецсимволы, знаки пунктуации и приводим к нижнему регистру
-    text = re.sub('[^A-Za-zА-Яа-я0-9]+', ' ', text)
-    text = text.lower()
+            # увеличиваем статистику
+            with self.stats_lock:
+                self.stats['total_processed'] += 1
+                print(f"Worker {self.id} processed {url}")
+                print(f"Total processed: {self.stats['total_processed']}")
 
-    # Разбиваем текст на слова и считаем количество вхождений каждого слова
-    words = text.split()
-    word_count = Counter(words)
+    def get_words(self, url):
+        # Отправляем GET-запрос по указанному URL
+        response = requests.get(url)
+        # Парсим HTML-код и получаем текст
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = soup.get_text()
+        
+        # Удаляем спецсимволы, знаки пунктуации и приводим к нижнему регистру
+        text = re.sub('[^A-Za-zА-Яа-я0-9]+', ' ', text)
+        text = text.lower()
 
-    # Получаем топ k наиболее часто встречающихся слов
-    top_k_words = dict(word_count.most_common(k))
+        # Разбиваем текст на слова и считаем количество вхождений каждого слова
+        words = text.split()
+        word_count = Counter(words)
 
-    # Возвращаем результат в формате JSON
-    return json.dumps(top_k_words, ensure_ascii=False)
+        # Получаем топ k наиболее часто встречающихся слов
+        top_k_words = dict(word_count.most_common(self.k))
+
+        # Возвращаем результат в формате JSON
+        return json.dumps(top_k_words, ensure_ascii=False)
+
+
+class MasterServer:
+    def __init__(self, host, port, workers, k):
+        self.host = host
+        self.port = port
+        self.k = k
+        self.workers = workers
+        self.task_queue = Queue()
+        self.stats_lock = threading.Lock()
+        self.stats = {
+            'total_processed': 0
+        }
+
+    def run(self):
+        # запускаем мастер-сервер на прослушивание запросов
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))
+            s.listen()
+
+            print(f"Master server started at {self.host}:{self.port} with {self.workers} workers")
+
+            # создаем воркеров и запускаем их
+            for i in range(self.workers):
+                worker = Worker(i + 1, self.task_queue, self.stats, self.stats_lock, self.k)
+                worker.start()
+
+            # принимаем запросы от клиентов
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    print(f"Connected by {addr}")
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        url = data.decode("utf-8").strip()
+                        # print(f"Received URL: {url}")
+                        self.task_queue.put((conn, url))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w', type=int, default=1)
-    parser.add_argument('-k', type=int, default=1)
+    parser.add_argument('-w', type=int, default=5)
+    parser.add_argument('-k', type=int, default=3)
     args = parser.parse_args()
-    url = 'https://en.wikipedia.org/wiki/Lionel_Messi'
-    print(get_words(url, args.k))
+
+    server = MasterServer('localhost', 8000, args.w, args.k)
+    server.run()
